a68b56152b35234505af4d1c02f57545437585a7
doctavscm <galslot@gmail.com>
2025-02-02 10:09:36 +0300

    Конфигурация. autoload.

---
+++ web/config/init.php
@@ -0,0 +1,26 @@
+<?php
+
+
+    define("DEBUG", 1); // 0 - Prod.
+
+    define("ROOT", dirname(__DIR__));
+    define("WWW", ROOT. "/public");
+    define("APP", ROOT. "/app");
+    define("CORE", ROOT. "/core");
+    define("HELPERS", ROOT. "/core/helpers");
+    define("CACHE", ROOT. "/var/cache");
+    define("LOG", ROOT. "/var/log");
+    define("CONFIG", ROOT. "/config");
+    define("LAYOUT", ROOT. "/shop1");
+    define("PATH", ROOT. "http://localhost");
+    define("ADMIN", ROOT. "http://localhost/admin");
+    define("UPLOADS", ROOT. "/public/uploads");
+    define("NO_IMAGE", "uploads/no_image.jpg");
+
+    define("DB_HOST", "db8");
+    define("DB_USER", "root");
+    define("DB_PASS", "123456");
+    define("DB_NAME", "framework8");
+
+    require_once ROOT. "/vendor/autoload.php";
+
--- web/public/index.php
+++ web/public/index.php
@@ -1,5 +1,10 @@
 <?php
 
- phpinfo();
+if(PHP_MAJOR_VERSION < 8){
+    die("Необходима версия PHP >= 8");
+}
 
- 
+require_once dirname(__DIR__) . '/config/init.php';
+
+echo "App Start";
+
